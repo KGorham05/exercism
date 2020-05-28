1411a1d7a8bdbfed6cdbe0883ad8288bd5b481aa
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 12:13:26 -0700

    changed hard coded array to instead use .first(n) and .map

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 class ResistorColorDuo
   COLOR_VALUES = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(color_arr)
-    [COLOR_VALUES.index(color_arr[0]), COLOR_VALUES.index(color_arr[1])].join().to_i
+    color_arr.first(2).map { |color| COLOR_VALUES.index(color) }.join().to_i
   end
 end
 
