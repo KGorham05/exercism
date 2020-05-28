8b46d7d9b362675046233fb47af3a413c5ecdec2
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 11:07:04 -0700

    refactored value method so that it uses an array and .join to convert the values to strings and concatenate them

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,6 +1,7 @@
 class ResistorColorDuo
   COLOR_VALUES = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(color_arr)
-    (COLOR_VALUES.index(color_arr[0]).to_s + COLOR_VALUES.index(color_arr[1]).to_s).to_i
+    value = [COLOR_VALUES.index(color_arr[0]), COLOR_VALUES.index(color_arr[1])].join().to_i
   end
 end
+
