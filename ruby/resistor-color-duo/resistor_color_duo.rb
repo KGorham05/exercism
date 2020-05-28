8d3da02b1b33931fd8b6f7687681337fb35d353e
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 11:19:59 -0700

    added javascript and elixir tracks

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 class ResistorColorDuo
   COLOR_VALUES = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(color_arr)
-    value = [COLOR_VALUES.index(color_arr[0]), COLOR_VALUES.index(color_arr[1])].join().to_i
+    [COLOR_VALUES.index(color_arr[0]), COLOR_VALUES.index(color_arr[1])].join().to_i
   end
 end
 
