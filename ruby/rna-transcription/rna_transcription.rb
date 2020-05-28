be695d8be56d88756f695c5590412106e3e5b79f
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-27 17:10:03 -0700

    RNA tests passing

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -6,3 +6,22 @@ To get started with TDD, see the `README.md` file in your
 `ruby/rna-transcription` directory.
 =end
 
+class Complement
+
+  def self.of_dna(string)
+    @converted = ""
+    string.each_char do |x| 
+      if x == "G"
+        @converted += "C"
+      elsif x == "C"
+        @converted += "G"
+      elsif x == "T"  
+        @converted += "A"
+      elsif x == "A"  
+        @converted += "U"  
+      end
+    end
+    @converted
+  end
+
+end
