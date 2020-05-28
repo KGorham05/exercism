3638b0fa2e10f13193ce13851b3cedcb29b96303
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 16:35:27 -0700

    removed white space and comments

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,5 @@
-# Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
-
 class Acronym
-
   def self.abbreviate(string)
     string.sub("-", " ").split.map { |word| word[0] }.join().upcase
   end
-
 end
--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,12 +1,3 @@
-# Given a year, report if it is a leap year.
-
-# A leap year occurs:
-
-# on every year that is evenly divisible by 4
-#   except every year that is evenly divisible by 100
-#     unless the year is also evenly divisible by 400
-# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is
-
 class Year
   def self.leap?(year) 
     year % 4 == 0 unless (year % 100 == 0 unless year % 400 == 0)   
