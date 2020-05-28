b1e654d0ad15a6595272f912a82136c3f8649999
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 16:34:51 -0700

    udpated leap to use 'unless' - ruby

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,8 +1,14 @@
-=begin
-Write your code for the 'Leap' exercise in this file. Make the tests in
-`leap_test.rb` pass.
+# Given a year, report if it is a leap year.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/leap` directory.
-=end
+# A leap year occurs:
 
+# on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is
+
+class Year
+  def self.leap?(year) 
+    year % 4 == 0 unless (year % 100 == 0 unless year % 400 == 0)   
+  end
+end
