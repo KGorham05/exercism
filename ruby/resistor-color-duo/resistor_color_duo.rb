beaf9b6079531eda054d981e6514bb991b995540
Gorham <Kevin.Gorham@djoglobal.com>
2020-05-28 09:32:00 -0700

    improved resistor color duo exercise with mentor feedback

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,9 +1,6 @@
-
 class ResistorColorDuo
-  
+  COLOR_VALUES = %w[black brown red orange yellow green blue violet grey white].freeze
   def self.value(color_arr)
-    @color_values = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-    (@color_values.index(color_arr[0]).to_s + @color_values.index(color_arr[1]).to_s).to_i
+    (COLOR_VALUES.index(color_arr[0]).to_s + COLOR_VALUES.index(color_arr[1]).to_s).to_i
   end
-
 end
