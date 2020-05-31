78aee1bffffe0ff9ac0b3a13513867279aada808
Gorham <kevin.gorham@gmail.com>
2020-05-31 15:03:01 -0700

    updated variable name in acronym exercise

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,16 +1,16 @@
 # Solution 1
 
 # class Acronym
-#   def self.abbreviate(string)
-#     string.sub("-", " ").split.map { |word| word[0] }.join().upcase
+#   def self.abbreviate(phrase)
+#     phrase.sub("-", " ").split.map { |word| word[0] }.join().upcase
 #   end
 # end
 
 # Solution 2 - using scan and regex
 
 class Acronym
-  def self.abbreviate(string)
-    string.scan(/\b[a-zA-Z]/).join().upcase
+  def self.abbreviate(phrase)
+    phrase.scan(/\b[a-zA-Z]/).join().upcase
   end
 end
   
