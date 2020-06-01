f9fee9fdc1cda2d1b7a57d7f7e7dd285b7865b97
Gorham <kevin.gorham@gmail.com>
2020-06-01 15:33:49 -0700

    v1 of space-age ruby exercise complete

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,46 +1,30 @@
 class SpaceAge
   def initialize(age_in_seconds) 
-    @age_in_seconds = age_in_seconds
+    @age_on_earth =  age_in_seconds / 31556700.to_f
   end
-
-  # Mercury: orbital period 0.2408467 Earth years
   def on_mercury
-    
+    @age_on_earth / 0.2408467
   end
-
-  # Venus: orbital period 0.61519726 Earth years
   def on_venus
-
+    @age_on_earth / 0.61519726
   end
-
-  # Earth: orbital period 365.25 Earth days, or 31557600 seconds
+  57600 seconds
   def on_earth
-    @age_on_earth =  @age_in_seconds / 31556700.0 
+    @age_on_earth
   end
-  
-  # Mars: orbital period 1.8808158 Earth years
-  def on_mars
-
+  f on_mars
+    @age_on_earth / 1.8808158
   end
-
-  # Jupiter: orbital period 11.862615 Earth years
-  def on_jupiter
-
+   def on_jupiter
+    @age_on_earth / 11.862615
   end
-
-  # Saturn: orbital period 29.447498 Earth years
   def on_saturn
-
+    @age_on_earth / 29.447498
   end
-
-  # Uranus: orbital period 84.016846 Earth years
   def on_uranus
-
+    @age_on_earth / 84.016846
   end
-
-  # Neptune: orbital period 164.79132 Earth years
-  def on_neptune
-
+   def on_neptune
+    @age_on_earth / 164.79132
   end
-
 end
