af99c4316c672c27f9519e9e07a42870f9746901
Gorham <kevin.gorham@gmail.com>
2020-06-01 15:42:46 -0700

    reverted accidental deletion

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,30 +1,38 @@
 class SpaceAge
+
   def initialize(age_in_seconds) 
     @age_on_earth =  age_in_seconds / 31556700.to_f
   end
+
   def on_mercury
     @age_on_earth / 0.2408467
   end
+
   def on_venus
     @age_on_earth / 0.61519726
   end
-  57600 seconds
+
   def on_earth
     @age_on_earth
   end
-  f on_mars
+
+  def on_mars
     @age_on_earth / 1.8808158
   end
-   def on_jupiter
+
+  def on_jupiter
     @age_on_earth / 11.862615
   end
+
   def on_saturn
     @age_on_earth / 29.447498
   end
+
   def on_uranus
     @age_on_earth / 84.016846
   end
-   def on_neptune
+
+  def on_neptune
     @age_on_earth / 164.79132
   end
 end
