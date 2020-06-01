25aea2b312c9df32fe6ca04803c93425f5f8150f
Gorham <kevin.gorham@gmail.com>
2020-06-01 16:25:53 -0700

    got v1 of protein-translation javascript exercise working

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -11,50 +11,24 @@ export const translate = (codons) => {
     "STOP" : ["UAA", "UAG", "UGA"]
   }
 
-  let codonArr = codons.match
+  let proteins = []
 
-  // let proteins = codons.map(codon => {
-  //   PROTEIN_MAP.
-  // });
+  if (codons) {
+    let codon_arr = codons.match(/.{1,3}/g)
+    for (let i = 0; i < codon_arr.length; i++) {
+      let protein = Object.keys(PROTEIN_MAP).find(key => PROTEIN_MAP[key].includes(codon_arr[i]))
+      console.log(protein)
+      if (!protein) {
+        throw "Invalid codon"
+      } else if (protein == "STOP") {
+        break
+      } else {
+        proteins.push(protein)
+      }
+    }
+  }
   
-
   return proteins;
-  
 };
 
-translate();
-// Translate RNA sequences into proteins.
-
-// RNA can be broken into three nucleotide sequences called codons, and then translated to a polypeptide like so:
-
-// RNA: "AUGUUUUCU" => translates to
-
-// Codons: "AUG", "UUU", "UCU" => which become a polypeptide with the following sequence =>
-
-// Protein: "Methionine", "Phenylalanine", "Serine"
-
-// There are 64 codons which in turn correspond to 20 amino acids; however, all of the codon sequences and resulting amino acids are not important in this exercise. If it works for one codon, the program should work for all of them. However, feel free to expand the list in the test suite to include them all.
-
-// There are also three terminating codons (also known as 'STOP' codons); if any of these codons are encountered (by the ribosome), all translation ends and the protein is terminated.
-
-// All subsequent codons after are ignored, like this:
-
-// RNA: "AUGUUUUCUUAAAUG" =>
-
-// Codons: "AUG", "UUU", "UCU", "UAA", "AUG" =>
-
-// Protein: "Methionine", "Phenylalanine", "Serine"
-
-// Note the stop codon "UAA" terminates the translation and the final methionine is not translated into the protein sequence.
-
-// Below are the codons and resulting Amino Acids needed for the exercise.
-
-// Codon	Protein
-// AUG	Methionine
-// UUU, UUC	Phenylalanine
-// UUA, UUG	Leucine
-// UCU, UCC, UCA, UCG	Serine
-// UAU, UAC	Tyrosine
-// UGU, UGC	Cysteine
-// UGG	Tryptophan
-// UAA, UAG, UGA	STOP
+translate('UAAUUUUUA')
