2248c1bdb08af8e0584c95b17c3421adda6fd878
Gorham <kevin.gorham@gmail.com>
2020-06-06 15:42:14 -0700

    updated second .map in matrix.rb to use (&:to_i) syntax

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,7 +2,7 @@ class Matrix
   attr_reader :rows, :columns
 
   def initialize(text)
-    @rows = text.lines.map {|line| line.split(" ").map {|num| num.to_i}}
+    @rows = text.lines.map {|line| line.split(" ").map(&:to_i)}
     @columns = rows.transpose
   end
 
