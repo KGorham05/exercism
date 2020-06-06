4daaaead149a14eef86978eb7801626e85e57d4e
Gorham <kevin.gorham@gmail.com>
2020-06-06 12:40:57 -0700

    updated resistor-color-duo javascript activity to be 1 line

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,8 +1,11 @@
 const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
-export const decodedValue = (colors) => {
-  const firstValue = COLORS.indexOf(colors.shift())
-  const secondValue = COLORS.indexOf(colors.shift())
-  return parseInt(`${firstValue}${secondValue}`)
-};
+// export const decodedValue = (colors) => {
+//   const firstValue = COLORS.indexOf(colors.shift())
+//   const secondValue = COLORS.indexOf(colors.shift())
+//   return parseInt(`${firstValue}${secondValue}`)
+// };
 
+export const decodedValue = (colors) => {
+  return COLORS.indexOf(colors.shift()) * 10 + COLORS.indexOf(colors.shift());
+};
