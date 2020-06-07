e7310ddb0927b0fa21f9cd6cd9f6ed92848ebfc3
Kgor <kevin.gorham@gmail.com>
2020-06-06 22:22:38 -0700

    difference_of_squares.rb first draft complete

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,8 +1,20 @@
-=begin
-Write your code for the 'Difference Of Squares' exercise in this file. Make the tests in
-`difference_of_squares_test.rb` pass.
+class Squares
+  attr_reader :num_array
 
-To get started with TDD, see the `README.md` file in your
-`ruby/difference-of-squares` directory.
-=end
+  def initialize(num)
+    @num_array = [*1..num]
+  end
 
+  def square_of_sum
+    num_array.sum**2  
+  end
+
+  def sum_of_squares
+    num_array.map {|num| num**2}.sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+end
