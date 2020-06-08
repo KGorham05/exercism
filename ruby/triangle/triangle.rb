d6e8f97aa35881f44062d62f56628fb9928af3c5
Kgor <kevin.gorham@gmail.com>
2020-06-07 21:41:15 -0700

    working on ruby triange.rb exercise

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,8 +1,41 @@
-=begin
-Write your code for the 'Triangle' exercise in this file. Make the tests in
-`triangle_test.rb` pass.
+# Triangle
+# Determine if a triangle is equilateral, isosceles, or scalene.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/triangle` directory.
-=end
+# An equilateral triangle has all three sides the same length.
 
+# An isosceles triangle has at least two sides the same length. (It is sometimes specified as having exactly two sides the same length, but for the purposes of this exercise we'll say at least two.)
+
+# A scalene triangle has all sides of different lengths.
+
+# Note
+# For a shape to be a triangle at all, all sides have to be of length > 0, and the sum of the lengths of any two sides must be greater than or equal to the length of the third side. See Triangle Inequality.
+
+# Dig Deeper
+# The case where the sum of the lengths of two sides equals that of the third is known as a degenerate triangle - it has zero area and looks like a single line. Feel free to add your own code/tests to check for degenerate triangles.
+# class Trinangle
+
+# end
+
+class Triangle
+  attr_reader :sides
+
+  def initialize(side_lengths)
+    @sides = side_lengths
+  end
+
+  def equilateral?
+    if sides.all? {|n| n != 0}
+      sides.uniq.size == 1 
+    end
+  end
+
+  # def isosceles?
+  
+  # end
+
+  # def scaline?
+  
+  # end
+
+
+end
