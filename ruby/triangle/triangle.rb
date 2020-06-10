c09938ccb4d2b6da7f04c20422ed6ac68f17b44a
Gorham <kevin.gorham@gmail.com>
2020-06-10 14:54:05 -0700

    triangle.rb complete!

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,26 +1,14 @@
-# Triangle
-# Determine if a triangle is equilateral, isosceles, or scalene.
-
-# An equilateral triangle has all three sides the same length.
-
-# An isosceles triangle has at least two sides the same length. (It is sometimes specified as having exactly two sides the same length, but for the purposes of this exercise we'll say at least two.)
-
-# A scalene triangle has all sides of different lengths.
-
-# Note
-# For a shape to be a triangle at all, all sides have to be of length > 0, and the sum of the lengths of any two sides must be greater than or equal to the length of the third side. See Triangle Inequality.
-
-# Dig Deeper
-# The case where the sum of the lengths of two sides equals that of the third is known as a degenerate triangle - it has zero area and looks like a single line. Feel free to add your own code/tests to check for degenerate triangles.
-# class Trinangle
-
-# end
-
 class Triangle
   attr_reader :sides
 
   def initialize(side_lengths)
     @sides = side_lengths
+  end
+
+  def isTriangle? 
+    sorted_sides = sides.sort
+    greatest_value = sorted_sides.pop
+    greatest_value <= sorted_sides.sum && (sides.reject{|side| side == 0} == sides) 
   end
 
   def equilateral?
@@ -30,12 +18,13 @@ class Triangle
   end
 
   def isosceles?
-    sides.uniq.size == 1 || 2
+    sides.uniq.size == 1 || sides.uniq.size == 2 && isTriangle?
   end
 
-  # def scaline?
-  
-  # end
-
+  def scalene?
+    sides.uniq.size == 3 && isTriangle?
+  end
 
 end
+
+
