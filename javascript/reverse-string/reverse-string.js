c3a10d55154193fac885d3a2bebae10ade5cba0a
Gorham <kevin.gorham@gmail.com>
2020-06-10 17:19:15 -0700

    added recursive solution to reverse-string.js

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,3 +1,4 @@
+// Solution 1 - while (i--)
 // export const reverseString = (text) => {
 //   let textReversed = ''
 //   let i = text.length 
@@ -7,6 +8,11 @@
 //   return textReversed
 // };
 
+// Solution 2 - .reverse()
 // export const reverseString = (text) => text.split('').reverse().join('')
 
-export const reverseString = (text) => [...text].reverse().join('')
+// Solution 2 - spread operator + reverse
+// export const reverseString = (text) => [...text].reverse().join('')
+
+// Solution 3 - Recursive Ternary
+export const reverseString = (text) => text === '' ? '' : reverseString(text.substr(1)) + text.charAt(0)
