5342a0ddd2949e92f003d89f387b53c89b7b4cd1
Gorham <kevin.gorham@gmail.com>
2020-06-10 17:06:08 -0700

    refactored reverse-string.js

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,8 +1,12 @@
-export const reverseString = (text) => {
-  let textReversed = ''
-  let i = text.length 
-  while (i--) {
-    textReversed += text[i];
-  }
-  return textReversed
-};
+// export const reverseString = (text) => {
+//   let textReversed = ''
+//   let i = text.length 
+//   while (i--) {
+//     textReversed += text[i];
+//   }
+//   return textReversed
+// };
+
+// export const reverseString = (text) => text.split('').reverse().join('')
+
+export const reverseString = (text) => [...text].reverse().join('')
