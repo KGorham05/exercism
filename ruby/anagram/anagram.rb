375d40f3c491ccd1d0d599b7b4414ed6ff5b90fe
Gorham <kevin.gorham@gmail.com>
2020-09-15 16:01:07 -0700

    first draft of anagram method working

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -7,24 +7,46 @@
 class Anagram
 
   def initialize(word)
-    @chars = word.chars
-    @num_unique_chars = @unique_chars.uniq.length
+    # downcase to account for mixed casing
+    @word_to_match = word.downcase
+    @chars = word.downcase.chars
+    @num_unique_chars = @chars.uniq.length
   end
 
-  def match(arr)
-    # loop over the array
-    # turn the word into an array
-    # make sure each character is found in the word array
-    # if all the characters in the original word find a match
-    # confirm the lenght's match
-    # confirm the uniq length matches
-    # add that word to the array of matches
+  def match(possible_anagrams)
+    confirmed_anagrams = []
+    # loop over the array && confirm the uniq length matches
+    possible_anagrams.map{ |word| 
+      # confirm the lenght's match
+      if word.length == @chars.length && word.chars.uniq.length == @num_unique_chars 
+        # turn the word into an array
+        num_to_confirm_match = word.length
+        num_of_confirmed_chars = 0
+        word.chars.map{ |char| 
+          # make sure each character is found in the word array
+          # downcase to account for mixed casing
+          if @chars.include?(char.downcase)
+            # make sure it occurs the same number of times
+            if @chars.count(char.downcase) == word.count(char)
+              num_of_confirmed_chars += 1
+            end
+          end
+        }
+        # if all the characters in the original word find a match
+        if num_of_confirmed_chars == num_to_confirm_match
+          # Make sure not to push anagrams of itself 
+          unless word.downcase == @word_to_match
+            # add that word to the array of matches
+            confirmed_anagrams.push(word)
+          end
+        end
+      end      
+    }
     # after the loop, return the array of matched words
-    p arr
+    confirmed_anagrams
   end
-
 end
 
-detector = Anagram.new('master')
-anagrams = detector.match(["stream", "pigeon", "maters"])
+# detector = Anagram.new('master')
+# anagrams = detector.match(["stream", "pigeon", "maters", "sserpn"])
 # expected = ["maters", "stream"]
