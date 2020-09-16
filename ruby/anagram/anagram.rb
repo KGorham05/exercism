b7150e42d7bfcf1e0f5f1ed5e6a4acafb9072682
Gorham <kevin.gorham@gmail.com>
2020-09-16 08:35:07 -0700

    added v2 solution after help from Derek Croft

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -4,48 +4,54 @@
 # "inlets" "banana"` the program should return a list containing
 # `"inlets"`.
 
+# V2 Solutions (with help)
 class Anagram
 
   def initialize(word)
-    # downcase to account for mixed casing
-    @word_to_match = word.downcase
-    @chars = word.downcase.chars
-    @num_unique_chars = @chars.uniq.length
+    @word_to_match = word
   end
 
   def match(possible_anagrams)
-    confirmed_anagrams = []
-    # loop over the array && confirm the uniq length matches
-    possible_anagrams.map{ |word| 
-      # confirm the lenght's match
-      if word.length == @chars.length && word.chars.uniq.length == @num_unique_chars 
-        # turn the word into an array
-        num_to_confirm_match = word.length
-        num_of_confirmed_chars = 0
-        word.chars.map{ |char| 
-          # make sure each character is found in the word array
-          # downcase to account for mixed casing
-          if @chars.include?(char.downcase)
-            # make sure it occurs the same number of times
-            if @chars.count(char.downcase) == word.count(char)
-              num_of_confirmed_chars += 1
-            end
-          end
-        }
-        # if all the characters in the original word find a match
-        if num_of_confirmed_chars == num_to_confirm_match
-          # Make sure not to push anagrams of itself 
-          unless word.downcase == @word_to_match
-            # add that word to the array of matches
-            confirmed_anagrams.push(word)
-          end
-        end
-      end      
-    }
-    # after the loop, return the array of matched words
-    confirmed_anagrams
-  end
+    possible_anagrams.select do |word| 
+      unless word.downcase == @word_to_match.downcase
+        word.downcase.chars.sort == @word_to_match.downcase.chars.sort
+      end
+    end
+   end
 end
+
+# V1 Solution
+# class Anagram
+
+#   def initialize(word)
+#     @word_to_match = word.downcase
+#     @chars = word.downcase.chars
+#     @num_unique_chars = @chars.uniq.length
+#   end
+
+#   def match(possible_anagrams)
+#     confirmed_anagrams = []
+#     possible_anagrams.map{ |word| 
+#       if word.length == @chars.length && word.chars.uniq.length == @num_unique_chars 
+#         num_to_confirm_match = word.length
+#         num_of_confirmed_chars = 0
+#         word.chars.map{ |char|     
+#           if @chars.include?(char.downcase)
+#             if @chars.count(char.downcase) == word.count(char)
+#               num_of_confirmed_chars += 1
+#             end
+#           end
+#         }
+#         if num_of_confirmed_chars == num_to_confirm_match
+#           unless word.downcase == @word_to_match
+#             confirmed_anagrams.push(word)
+#           end
+#         end
+#       end      
+#     }
+#     confirmed_anagrams
+#   end
+# end
 
 # detector = Anagram.new('master')
 # anagrams = detector.match(["stream", "pigeon", "maters", "sserpn"])
