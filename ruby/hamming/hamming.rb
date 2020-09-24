c80aeed5791f43ffbea5d8d2bba26606655a2cad
Gorham <kevin.gorham@gmail.com>
2020-09-24 15:40:27 -0700

    updated camelCase to snake_case

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,16 +2,16 @@
 #v2 Recursive Solution
 module Hamming
 
-  def self.checkEqualLength(s1, s2)
+  def self.check_equal_length(s1, s2)
     if s1.length != s2.length 
       raise ArgumentError.new("Must input two equal length strings to compute Hamming difference")
     end
   end
 
-  def self.compute(s1, s2, i = 0, total = 0, lengthHasBeenChecked = false)
-    unless lengthHasBeenChecked
-      checkEqualLength(s1, s2)
-      lengthHasBeenChecked = true
+  def self.compute(s1, s2, i = 0, total = 0, length_has_been_checked = false)
+    unless length_has_been_checked
+      check_equal_length(s1, s2)
+      length_has_been_checked = true
     end
 
     if i == s1.length
@@ -20,7 +20,7 @@ module Hamming
       total += 1
     end
     i += 1
-    compute(s1, s2, i, total, lengthHasBeenChecked)
+    compute(s1, s2, i, total, length_has_been_checked)
   end
 
 end
