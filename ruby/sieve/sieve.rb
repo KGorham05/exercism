4c70b965716cff15eb56665756a2993bd6d6f26a
Gorham <kevin.gorham@gmail.com>
2020-10-01 16:28:34 -0700

    Updated sieve.rb solution

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,26 +1,43 @@
+# v2
 class Sieve
-  attr_accessor :limit, :digits_to_check
 
-  def initialize(num_limit)
-    @limit = num_limit
-    @digits_to_check = (2..limit).to_a
+  def initialize(limit)
+    @limit = limit
   end
 
   def primes 
-    primes = []
-    digits_to_check.each { |num| 
-      primes.push(num)
-      multiple = 2
-      product = num * 1
-      while product < limit do
-        product = num * multiple
-        if digits_to_check.include?(product)
-          digits_to_check.delete(product)
-        end
-        multiple += 1
-      end
-    }
-    primes
+    primes = [nil, nil, *2..@limit]
+    (2..@limit).each do |i|
+      (i**2..@limit).step(i) {|m| primes[m] = nil } if primes[i]
+    end
+    primes.compact
   end
 
 end
+
+# v1
+# class Sieve
+
+#   def initialize(num_limit)
+#     @limit = num_limit
+#     @digits_to_check = (2..limit).to_a
+#   end
+
+#   def primes 
+#     primes = []
+#     @digits_to_check.each { |i| 
+#       primes.push(i)
+#       multiple = 2
+#       product = num * 1
+#       while product < @limit do
+#         product = num * multiple
+#         if @digits_to_check.include?(product)
+#           @digits_to_check.delete(product)
+#         end
+#         multiple += 1
+#       end
+#     }
+#     primes
+#   end
+
+# end
