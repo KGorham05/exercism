afaf1f18fd4a1565901856c756d3b56ddfc3db05
Gorham <kevin.gorham@gmail.com>
2020-10-06 12:22:00 -0700

    added nucleotide_count.rb solution

---
+++ ruby/nucleotide-count/nucleotide_count.rb
@@ -0,0 +1,24 @@
+class Nucleotide
+  NUCLEOTIDES = %w[A T C G].freeze
+
+  def initialize(dna)
+    @dna = dna
+    raise ArgumentError unless dna.chars.all? { |char| NUCLEOTIDES.include?(char) }
+  end
+
+  def self.from_dna(dna)
+    new(dna)
+  end
+
+  def count(nucleotide)
+    @dna.count(nucleotide)
+  end
+  
+  def histogram
+    NUCLEOTIDES.each_with_object ({}) { |nucleotide, histogram| 
+      histogram[nucleotide] = @dna.count(nucleotide)
+    }
+  end
+
+end
+
