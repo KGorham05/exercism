b85361299519f55651e9d8d8752fb9d828a7c1c9
Gorham <kevin.gorham@gmail.com>
2020-10-25 15:20:57 -0700

    gigasecond.js complete

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,2 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const gigasecond = (moment) => new Date(Date.parse(moment) + 1000000000000);
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
-};
