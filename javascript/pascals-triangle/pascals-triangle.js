a77669cf29004a28a59d65138095c3c9415d31ab
Gorham <kevin.gorham@gmail.com>
2020-10-27 16:15:37 -0700

    pascals-triangle.js v2

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,17 +1,38 @@
-// v1 Recursive
-export const rows = (height, pyramid = [], i = 0) => {
-  if (pyramid.length === height) {
-    return pyramid;
+// v2 for loop
+export const rows = (height) => {
+  const pyramid = [];
+  for (let i = 0; i <= height; i++) {
+    if (pyramid.length === height) {
+      return pyramid;
+    }
+    if (pyramid.length === 0) {
+      pyramid.push([1]);
+      continue;
+    }
+    const newRow = [1];
+    const lastRow = pyramid[i - 1];
+    lastRow.forEach((value, j) => {
+      if (lastRow[j + 1]) newRow.push(lastRow[j] + lastRow[j + 1]);
+    });
+    newRow.push(1);
+    pyramid.push(newRow);
   }
-  if (pyramid.length === 0) {
-    return rows(height, [[1]], ++i);
-  }
-  const newRow = [1];
-  const lastRow = pyramid[i - 1];
-  lastRow.forEach((value, j) => {
-    if (lastRow[j + 1]) newRow.push(lastRow[j] + lastRow[j + 1]);
-  });
-  newRow.push(1);
-  pyramid.push(newRow);
-  return rows(height, pyramid, ++i);
 };
+
+// v1 Recursive
+// export const rows = (height, pyramid = [], i = 0) => {
+//   if (pyramid.length === height) {
+//     return pyramid;
+//   }
+//   if (pyramid.length === 0) {
+//     return rows(height, [[1]], ++i);
+//   }
+//   const newRow = [1];
+//   const lastRow = pyramid[i - 1];
+//   lastRow.forEach((value, j) => {
+//     if (lastRow[j + 1]) newRow.push(lastRow[j] + lastRow[j + 1]);
+//   });
+//   newRow.push(1);
+//   pyramid.push(newRow);
+//   return rows(height, pyramid, ++i);
+// };
