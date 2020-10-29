fdc97757ad0bc243bd5cddfb4505ddeec0ee716a
Gorham <kevin.gorham@gmail.com>
2020-10-29 14:23:32 -0700

    collatz_conjecture.rb complete

---
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -0,0 +1,10 @@
+class CollatzConjecture
+
+  def self.steps(n, steps = 0)
+    return steps if n == 1
+    raise ArgumentError.new('Must input a number > 0') if n < 1
+    n.even? ? n = n / 2 : n = 3 * n + 1
+    steps(n, steps += 1)
+  end
+
+end
