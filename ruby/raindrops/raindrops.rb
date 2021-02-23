e7030d75c6527db450f76a0d3f2b0b96a620d9ca
Kevin Gorham <kgorham@finetuneus.com>
2021-02-23 10:42:29 -0800

    raindrops v3

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,29 +2,10 @@
 class Raindrops
   def self.convert(n)
     converted = ""
-    if (n % 3 == 0)
-      converted += "Pling"
-    end
-    if (n % 5 == 0) 
-      converted += "Plang"
-    end
-    if (n % 7 == 0) 
-      converted +="Plong"
-    end
+    converted << "Pling" if n % 3 == 0
+    converted << "Plang" if n % 5 == 0
+    converted << "Plong" if n % 7 == 0
+
     converted.empty? ? n.to_s : converted
   end
 end
-
-# v2 - Shorter solution using .each, but less readable IMO
-# class Raindrops
-#   RAINDROPS = { 3 => "Pling", 5 => "Plang", 7 => "Plong" }
-
-#   def self.convert(n)
-#     converted = ""
-#     RAINDROPS.each { |prime, noise| converted += noise if n % prime == 0 }
-#     converted.empty? ? n.to_s : converted
-#   end
-# end
-
-
-
