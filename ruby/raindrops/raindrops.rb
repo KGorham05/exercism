212ae7b46df11ca8c49d9b255ff461827443f102
Kevin Gorham <kgorham@finetuneus.com>
2021-02-23 11:17:47 -0800

    minor tweaks to raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,9 +2,9 @@
 class Raindrops
   def self.convert(number)
     sound = ''
-    sound << 'Pling' if number % 3 == 0
-    sound << 'Plang' if number % 5 == 0
-    sound << 'Plong' if number % 7 == 0
+    sound << 'Pling' if (number % 3).zero?
+    sound << 'Plang' if (number % 5).zero?
+    sound << 'Plong' if (number % 7).zero?
 
     sound.empty? ? number.to_s : sound
   end
