fe8171dc283ceba1cdbc35e907db6ed7e033affd
Kevin Gorham <kgorham@finetuneus.com>
2021-02-23 11:15:25 -0800

    v4 raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,11 +1,11 @@
 # v1 - My first solution, easier to follow logic
 class Raindrops
   def self.convert(n)
-    converted = ""
-    converted << "Pling" if n % 3 == 0
-    converted << "Plang" if n % 5 == 0
-    converted << "Plong" if n % 7 == 0
+    sound = ''
+    sound << 'Pling' if n % 3 == 0
+    sound << 'Plang' if n % 5 == 0
+    sound << 'Plong' if n % 7 == 0
 
-    converted.empty? ? n.to_s : converted
+    sound.empty? ? n.to_s : sound
   end
 end
